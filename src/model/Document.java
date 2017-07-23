be31c3d3b86296eff54829b55c9438d512d02f72
Arne Philipeit <arne.philipeit@icloud.com>
2017-07-23 04:25:04 +0100

    Removed libary xml reading for simplicity and performance.

--- src/model/Document.java
+++ src/model/Document.java
@@ -2,13 +2,6 @@ package model;
 
 import java.io.*;
 
-import javax.xml.parsers.*;
-import javax.xml.transform.*;
-import javax.xml.transform.dom.*;
-import javax.xml.transform.stream.StreamResult;
-
-import org.w3c.dom.*;
-
 public class Document {
 	private String projectFolderPath;
 	private String languageFilePath;
@@ -25,171 +18,167 @@ public class Document {
 		languageFilePath = "";
 		translations = new Translations();
 		
+		String fileContents = "";
+		
 		try {
-			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		    factory.setValidating(true);
-		    
-		    DocumentBuilder builder = factory.newDocumentBuilder();
-		    
-		    org.w3c.dom.Document document = builder.parse(file);
-		    
-		    Node root = document.getChildNodes().item(0);
-		    
-		    if (root.getNodeName().equals("nano-translation") == false) {
-		    	return;
-		    }
-		    
-		    for (int i = 0; i < root.getChildNodes().getLength(); i++) {
-		    	Node node = root.getChildNodes().item(i);
-		    	
-		    	if (node.getNodeName().equals("project-folder")) {
-		    		projectFolderPath = node.getFirstChild().getNodeValue();
-		    		
-		    		if (projectFolderPath == null) {
-		    			projectFolderPath = "";
-		    		}
-		    		
-		    	}else if (node.getNodeName().equals("language-file")) {
-					languageFilePath = node.getFirstChild().getNodeValue();
+			BufferedReader reader = new BufferedReader(
+										new InputStreamReader(
+											new FileInputStream(file), 
+											"UTF-8"));
+			
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				while (line.startsWith("\t")) line = line.substring(1);
+				fileContents += line + "\n";
+			}
+			
+			reader.close();
+			
+		}catch (Exception e) {
+			return;
+		}
+		
+		String[] lines = fileContents.split("\n");
+		
+		for (int i = 0; i < lines.length; i++) {
+			String line = lines[i];
+			
+			if (line.startsWith("<project-folder>") && 
+				line.endsWith("</project-folder>")) {
+				projectFolderPath = line.substring(16, line.length() - 17);
+				
+			}else if (line.startsWith("<language-file>") && 
+					  line.endsWith("</language-file>")) {
+				languageFilePath = line.substring(15, line.length() - 16);
+				
+			}else if (line.equals("<languages>")) {
+				for (i++; i < lines.length; i++) {
+					line = lines[i];
 					
-					if (languageFilePath == null) {
-						languageFilePath = "";
-					}
-					
-				}else if (node.getNodeName().equals("languages")) {
-					Node languagesNode = node;
-					
-					for (int j = 0; j < languagesNode.getChildNodes().getLength(); j++) {
-						node = languagesNode.getChildNodes().item(j);
+					if (line.startsWith("<language>") && 
+						line.endsWith("</language>")) {
+						translations.addLanguage(line.substring(10, line.length() - 11));
 						
-						if (!node.getNodeName().equals("language")) continue;
-						
-						String language = node.getFirstChild().getNodeValue();
-						
-						if (language == null || language.equals("")) {
-							continue;
-						}
-						
-						if (translations.getIndexOfLanguage(language) == -1) {
-							translations.addLanguage(language);
-						}
-					}
-					
-				}else if (node.getNodeName().equals("entries")) {
-					Node translationsNode = node;
-					
-					for (int j = 0; j < translationsNode.getChildNodes().getLength(); j++) {
-						node = translationsNode.getChildNodes().item(j);
-						
-						if (!node.getNodeName().equals("entry")) continue;
-						
-						Node entry = node;
-						
-						String key = null;
-						
-						for (int k = 0; k < entry.getChildNodes().getLength(); k++) {
-							node = entry.getChildNodes().item(k);
-							
-							if (node.getNodeName().equals("key")) {
-								key = node.getFirstChild().getNodeValue();
-								
-								if (translations.getIndexOfKeyOfEntry(key) != -1) {
-									break;
-								}else{
-									translations.addEntry(key);
-								}
-								
-							}else if (node.getNodeName().equals("translation")) {
-								int index = translations.getIndexOfKeyOfEntry(key);
-								String language = node.getAttributes().getNamedItem("language").getNodeValue();
-								
-								if (index == -1 || translations.getIndexOfLanguage(language) == -1) continue;
-								
-								translations.setTranslationForLanguageOfEntryAtIndex(index, language, node.getFirstChild().getNodeValue());
-								
-							}else if (node.getNodeName().equals("information")) {
-								int index = translations.getIndexOfKeyOfEntry(key);
-								
-								if (index == -1) continue;
-								
-								translations.setInformationOfEntryAtIndex(index, node.getFirstChild().getNodeValue());
-							}
-						}
+					}else if (line.equals("</languages>")) {
+						break;
 					}
 				}
-		    }
-		    
-		}catch (Exception e) {}
+				
+			}else if (line.equals("<entries>")) {
+				for (i++; i < lines.length; i++) {
+					line = lines[i];
+					
+					if (line.startsWith("<entry>")) {
+						String key = null;
+						
+						for (i++; i < lines.length; i++) {
+							line = lines[i];
+							
+							if (line.startsWith("<key>") && 
+								line.endsWith("</key>")) {
+								key = line.substring(5, line.length() - 6);
+								
+								if (key.length() == 0) break;
+								if (translations.getIndexOfKeyOfEntry(key) != -1) break;
+								
+								translations.addEntry(key);
+								
+							}else if (line.equals("<translation>")) {
+								String language = null;
+								String translation = null;
+								
+								for (i++; i < lines.length; i++) {
+									line = lines[i];
+									
+									if (line.startsWith("<language>") && 
+										line.endsWith("</language>")) {
+										language = line.substring(10, line.length() - 11);
+										
+									}else if (line.startsWith("<value>") && 
+											  line.endsWith("</value>")) {
+										translation = line.substring(7, line.length() - 8);
+										
+									}else if (line.equals("</translation>")) {
+										break;
+									}
+								}
+								
+								if (key != null && language != null && translation != null) {
+									int index = translations.getIndexOfKeyOfEntry(key);
+									
+									translations.setTranslationForLanguageOfEntryAtIndex(index, language, translation);
+								}
+								
+							}else if (line.startsWith("<information>") && 
+									  line.endsWith("</information>")) {
+								String information = line.substring(13, line.length() - 14);
+								
+								if (key != null) {
+									int index = translations.getIndexOfKeyOfEntry(key);
+									
+									translations.setInformationOfEntryAtIndex(index, information);
+								}
+								
+							}else if (line.equals("</entry>")) {
+								break;
+							}
+						}
+						
+					}else if (line.equals("</entries>")) {
+						break;
+					}
+				}
+			}
+		}
 	}
 	
 	public void writeToFile(File file) {
-		projectFolderPath = "Hello!\n<a></a>\nBye!";
-		languageFilePath = "Hi!!!";
+		String fileContents = "";
 		
-	    try {
-			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder builder = factory.newDocumentBuilder();
-			
-			org.w3c.dom.Document document = builder.newDocument();
-			
-			Element nanoTranslation = document.createElement("nano-translation");
-			
-			Element projectFolderElement = document.createElement("project-folder");
-			projectFolderElement.appendChild(document.createTextNode(projectFolderPath));
-			nanoTranslation.appendChild(projectFolderElement);
-			
-			Element languageFileElement = document.createElement("language-file");
-			languageFileElement.appendChild(document.createTextNode(languageFilePath));
-			nanoTranslation.appendChild(languageFileElement);
-			
-			Element languagesElement = document.createElement("languages");
-			
-			for (int i = 0; i < translations.getNumberOfLanguages(); i++) {
-				Element languageElement = document.createElement("language");
-				languageElement.appendChild(document.createTextNode(translations.getLanguageAtIndex(i)));
-				languagesElement.appendChild(languageElement);
-			}
-			
-			nanoTranslation.appendChild(languagesElement);
-			
-			Element entriesElement = document.createElement("entries");
-			
-			for (int i = 0; i < translations.getNumberOfEntries(); i++) {
-				Element entryElement = document.createElement("entry");
-				entriesElement.appendChild(entryElement);
-				
-				Element keyElement = document.createElement("key");
-				keyElement.appendChild(document.createTextNode(translations.getKeyOfEntryAtIndex(i)));
-				entryElement.appendChild(keyElement);
-				
-				for (int j = 0; j < translations.getNumberOfLanguages(); j++) {
-					Element translationElement = document.createElement("translation");
-					translationElement.setAttribute("language", translations.getLanguageAtIndex(j));
-					translationElement.appendChild(document.createTextNode(translations.getTranslationForLanguageOfEntryAtIndex(i, j)));
-					entryElement.appendChild(translationElement);
-				}
-				
-				Element informationElement = document.createElement("information");
-				informationElement.appendChild(document.createTextNode(translations.getInformationOfEntryAtIndex(i)));
-				entryElement.appendChild(informationElement);
-			}
-			
-			nanoTranslation.appendChild(entriesElement);
-			
-			document.appendChild(nanoTranslation);
-			
-			TransformerFactory transformerFactory = TransformerFactory.newInstance();
-			Transformer transformer = transformerFactory.newTransformer();
-			
-			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-			
-			DOMSource source = new DOMSource(document);
-			
-			transformer.transform(source, new StreamResult(file));
-			
-		} catch (Exception e) {
-			e.printStackTrace();
+		fileContents += "<project-folder>" + projectFolderPath + "</project-folder>\n";
+		fileContents += "<language-file>" + languageFilePath + "</language-file>\n";
+		fileContents += "<languages>\n";
+		
+		for (String language : translations.getLanguages()) {
+			fileContents += "\t<language>" + language + "</language>\n";
 		}
+		
+		fileContents += "</languages>\n";
+		fileContents += "<entries>\n";
+		
+		for (int i = 0; i < translations.getNumberOfEntries(); i++) {
+			String key = translations.getKeyOfEntryAtIndex(i);
+			String information = translations.getInformationOfEntryAtIndex(i);
+			
+			fileContents += "\t<entry>\n";
+			fileContents += "\t\t<key>" + key + "</key>\n";
+			
+			for (String language : translations.getLanguages()) {
+				String translation = translations.getTranslationForLanguageOfEntryAtIndex(i, language);
+				
+				fileContents += "\t\t<translation>\n";
+				fileContents += "\t\t\t<language>" + language + "</language>\n";
+				fileContents += "\t\t\t<value>" + translation + "</value>\n";
+				fileContents += "\t\t</translation>";
+			}
+			
+			fileContents += "\t\t<information>" + information + "</information>\n";
+			fileContents += "\t</entry>\n";
+		}
+		
+		fileContents += "</entries>";
+		
+		try{
+			Writer writer = new BufferedWriter(
+								new OutputStreamWriter(
+									new FileOutputStream(file), 
+									"UTF-8"));
+			
+			writer.write(fileContents);
+			
+			writer.flush();
+			writer.close();
+		}catch (Exception ex) {}
 	}
 	
 	public File getProjectFolder() {
